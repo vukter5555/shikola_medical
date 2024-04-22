b7df8972d2460a47ccbed7855da680772d999f36
andrey-d-dimitrov <andrey.d.dimitrov.2023@elsys-bg.org>
2024-04-22 09:01:57 +0300

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+@app.route('/page4')
+def page4():
+    return render_template('page4.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
